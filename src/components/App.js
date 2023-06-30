d4a43308c9ff63189ed0e923a5c0697ada9339cc
MiriamPaternain <miriampaternain@gmail.com>
2023-06-30 18:57:47 +0200

    add basic html

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-//seccion import
-
-//.- de React, de archivos propios, Sass, Images
+import randmImage from '../img/randm.png'
 import '../styles/App.scss';
-
+import '../styles/Header.scss';
 /*  COMPONENTE */
 function App() {
   /*
     Variables de estado, funciones manejadoras de eventos, variables, funcion handle 
   */
   /* RETURN --> HTML */
-  return <div>pagina principal</div>;
+  return <div>
+    <header className='header'>
+      <img src={randmImage} alt="Rick and Morty portal shoes" className='header__img' />
+    </header>
+    <main className='main'>
+      <section className='main__filters'>
+        <form className="main__filter--byName"></form>
+      </section>
+      <section className='main__characters'>
+        <form className="main__characters--eachCaracter">
+          <ul>
+            <li></li>
+          </ul>
+        </form>
+      </section>
+    </main>
+  </div>;
 }
 
 /* export*/
