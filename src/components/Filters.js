4c3992cb242e1e9e7b3139bd092bdf3303944f4f
MiriamPaternain <miriampaternain@gmail.com>
2023-07-02 13:16:29 +0200

    avoid change route in filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,23 @@
-const Filters = ({filterByName, handleFilter}) => {
-     const handleChangeFilterName = (event) => {
+const Filters = ({ filterByName, handleFilter }) => {
+  const handleChangeFilterName = (event) => {
     handleFilter('name', event.target.value);
   };
+  const handleReturn = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className='main__filter'>
-          <input
-            type='text'
-            name='name_filter'
-            value={filterByName}
-            onChange={handleChangeFilterName}
-            placeholder='Busca aquí tu personaje'
-            className='main__filter--byName'
-          />
-        </form>
+    <form className='main__filter' onSubmit={handleReturn}>
+      <input
+        type='text'
+        name='name_filter'
+        value={filterByName}
+        onChange={handleChangeFilterName}
+        placeholder='Busca aquí tu personaje'
+        className='main__filter--byName'
+      />
+    </form>
+  );
+};
 
-  )
-}
-
-export default Filters
+export default Filters;
