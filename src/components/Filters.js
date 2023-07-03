5266898f7f91226293423df28a988bef74bc3f20
MiriamPaternain <miriampaternain@gmail.com>
2023-07-03 11:32:58 +0200

    add styles to filter by species

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -7,42 +7,44 @@ const Filters = ({ filterByName, handleFilter, filterBySpecie }) => {
   };
   const charactersNotFound = true;
   const handleFilterSpecie = (event) => {
-    handleFilter('species',event.target.value);
+    handleFilter('species', event.target.value);
   };
   return (
     <>
-      <form className='main__filter' onSubmit={handleReturn}>
-        <input
-          type='text'
-          name='name_filter'
-          value={filterByName}
-          onChange={handleChangeFilterName}
-          placeholder='Busca aquí tu personaje'
-          className='main__filter--byName'
-        />
-        {filterByName && charactersNotFound && (
-          <p className='notFound'>
-            No hay ningún personaje que coincida con la palabra{' '}
-            <span className='notFound--word'>{filterByName}</span>
-          </p>
-        )}
-      </form>
-      <label className='specieFilter' htmlFor='search_species'>
-        Escoge una especie
-        <select
-          className='specieFilter--box'
-          name='search_species'
-          id='search_species'
-          value={filterBySpecie}
-          onChange={handleFilterSpecie}
-        >
-          <option selected disbled value='ALL'>
-            Todas
-          </option>
-          <option value='Human'>Humano</option>
-          <option value='Alien'>Alien</option>
-        </select>
-      </label>
+      <div className="filterContainer">
+        <form className='main__filter' onSubmit={handleReturn}>
+          <input
+            type='text'
+            name='name_filter'
+            value={filterByName}
+            onChange={handleChangeFilterName}
+            placeholder='Busca aquí tu personaje'
+            className='main__filter--byName'
+          />
+          {filterByName && charactersNotFound && (
+            <p className='notFound'>
+              No hay ningún personaje que coincida con la palabra{' '}
+              <span className='notFound--word'>{filterByName}</span>
+            </p>
+          )}
+        </form>
+        <label className='speciesFilter' htmlFor='search_species'>
+          Escoge una especie
+          <select
+            className='speciesFilter--box'
+            name='search_species'
+            id='search_species'
+            value={filterBySpecie}
+            onChange={handleFilterSpecie}
+          >
+            <option selected disbled value='ALL' className="speciesFilter--all">
+              Todas
+            </option>
+            <option value='Human' className="speciesFilter--human">Humano</option>
+            <option value='Alien' className="speciesFilter--alien">Alien</option>
+          </select>
+        </label>
+      </div>
     </>
   );
 };
